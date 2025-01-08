9503a2c7086f985e55907f10f0b573c58eb3782f
Rejomy <iyaargg@gmail.com>
2025-01-08 18:11:08 +0500

    Update packet events version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,15 +11,15 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven("https://repo.codemc.io/repository/maven-releases/")
+    maven { url = uri("https://repo.codemc.io/repository/maven-releases/") }
+    maven { url = uri("https://repo.codemc.io/repository/maven-snapshots/") }
     maven ("https://repo.extendedclip.com/content/repositories/placeholderapi/")
     maven("https://oss.sonatype.org/content/repositories/snapshots")
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
 }
 
 dependencies {
-    compileOnly("com.github.retrooper:packetevents-spigot:2.4.0")
-    compileOnly("me.clip:placeholderapi:2.11.4")
+    compileOnly("com.github.retrooper:packetevents-spigot:2.7.0")
     compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
 
     compileOnly("org.projectlombok:lombok:1.18.34")
